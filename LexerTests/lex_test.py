97eef6467030e89cc5191068af4dfe9857cdacea
Panagiotis A <pargyropoulos@gmail.com>
2022-02-20 00:16:51 +0200

    another minor test

--- LexerTests/lex_test.py
+++ LexerTests/lex_test.py
@@ -3,13 +3,17 @@ from sample_game import txt
 
 print (100 * "*")
 print ("\nAscending....")
-x=Lexer()
-x.Tokenize(txt)
+
 Lex=Lexer(txt)
+game=0
 while not Lex.EOF:
-    print(Lex.GetToken)
+    token=Lex.GetToken
+    print(token)
+    if token['token_type']=='GAME_END':
+        game+=1
     Lex.MoveNext()
 
+print ("Total Games:",game)
 input()
 print ("\nDescending....")
 print (100 * "*")
