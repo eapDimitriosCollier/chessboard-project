ac2f63ea425802b65cf9498f17f97e1d493517bf
Panagiotis A <pargyropoulos@gmail.com>
2022-02-28 21:19:29 +0200

    sample game

---
+++ Lexer/sample_game.py
@@ -0,0 +1,291 @@
+txt="""
+[Event "GMA, Wijk aan Zee NED"] 
+[Site "?"]
+[Date "2003.??.??"]
+[Round "1"]
+[White "Anand,V"]
+[Black "Radjabov,T"]
+[Result "1/2"]
+[WhiteElo "2750"]
+[BlackElo "2620"]
+[ECO "C12"]
+[PlyCount "55"]
+[Annotator "Hathaway"]
+[cccc "sdfsdfs" ]
+1. e4 e6
+{ I'm not terribly familiar with the style of Radjabov, so I don't know if this is his usual opening. }
+2. d4 d5 3. Nc3 Nf6 (3...Bb4 
+{ The Winawer Variation is probably best, though not as easy to play. }) 4. Bg5
+{ threatens e4-e5xf6 }
+ (4. e5 
+{ keeps pieces on the board and avoids ...dxe4 }) 4...Bb4 (4...Be7 
+{ is more common and aims to trade dark-square bishops to ease Black's cramp }) (4...dxe4 
+{ aims to avoid any cramp by bringing pieces into alignment for trading, though White does get at least one very good piece (Ne4 or Bg5) and an easier time castling queen-side, to stir up king-side threats }
+ 5. Nxe4 Be7  (
+{ or Rubinstein's }
+ 5...Nbd7) ) 5. e5 h6 6. Bd2 (6. Bh4 g5 7. exf6 gxh4 
+{ Black seems to equalize a little easier after this as he can win Pf6 in exchange for Ph4. }) 6...Bxc3 (6...Nfd7 7. Qg4 
+{ and White isn't incurring any weaknesses, but is either gaining Bb4 for Nc3 or after ...Bb4-f8 Black is cramped again }
+  (7. Nb5 $5 Bxd2+ 8. Qxd2 a6 9. Na3) ) 7. bxc3 Ne4 8. Qg4
+{ White immediately takes aim at the backward Pg7 & Rh8 and usually Pf7 & Ke8. For the moment Bd2 serves to defend Pc3 and to prevent ...Qd8-g5 (offering a queen trade to end the pressure) . }
+ (
+{ While }
+ 8. h4 
+{ is often useful in the French Defense with this pawn structure, I don't know that it's been tried in this opening on this move. }) 8...g6 9. Bd3 (9. h4 
+{ could take over for Bd2 in guarding g5 and preparing a later attack by f2-f4, h4-h5 or vice versa. It also would allow Rh1 to develop to build the direct frontal threats to Pf7 & Pg6. }
+ 9...c5 10. Bd3 Nxd2 11. Kxd2 Qa5 12. dxc5 Qxc5 13. Ne2 Qxf2 $4 14. Raf1 Qc5 15. Bxg6 fxg6 16. Qxg6+)  (9. Qd1 
+{ Fritz7; Odd! }) 9...Nxd2 10. Kxd2 c5 11. Nf3
+{ This has been considered the main line for many years, but I wonder if White can allow ...c5-c4 and not use more pawns to fight through Black's pawns. }
+ (11. dxc5 
+{ is probably still wrong because of ...Qg5+ }) (11. h4 
+{ still makes sense }) 11...Bd7 (11...c4 $6 
+{ The problem with this is that however much it slows White, it also limits Black's queen-side offensive possibilities. }) (
+{ Prematurely playing }
+ 11...cxd4 
+{ lets White straighten-out his pawns and Black has made no real progress. }
+ 12. cxd4)  (11...Qa5 $5 
+{ Fritz7: with the idea of ...cxd4 }) 12. dxc5 Qe7 13. Rab1 Bc6 14. Nd4 Nd7
+{ These last few moves have been quite unusual for a French Defense, but they make sense; Qe7 defends Pf7 while Bc6 defends Pb7 and Nd7 threatens Pc5 & Pe5. }
+15. Rhe1 (15. Nxc6 bxc6 16. Rb7 Qxc5 17. Qf4 g5 18. Qd4 Qa5 19. Rb2 c5 $11 
+{ Fritz7 }) 15...Nxc5 16. Re3
+{ another way of getting the rook into position, in front of the king-side pawns, to threaten Black's king-side pawns }
+16...h5 17. Qg3 O-O-O
+{ After this it would seem Black's pieces can handle any threats White can generate. However, black might also have ideas of winning. How might he do that? Well, ...Be8, ...Kc8-b8-a8, ...Rd8-c8, ...Nc5-a4 and Pc3 is a target (slow I know) . Another idea is to keep Kd2 from ever escaping to safety by advancing ...h5-h4-h3 to break open the king-side and open the h-file for Black's rooks. }
+ (17...h4 $15 
+{ Fritz7 }) (17...Nxd3 $15 
+{ Fritz7 }) 18. Ke1 Qc7 (18...h4 19. Qg4 Rh5) 19. h4
+{ Anand aims to keep the king-side perfectly safe to ensure a draw. }
+ (19. Qh4 
+{ Fritz7 }) 19...Qa5 20. Kf1 (20. Nxc6 bxc6 21. Kf1 Kd7 20. Qf4 Ke8 $11 
+{ Fritz7 }) 20...Rd7 (
+{ Premature is }
+ 20...Qxa2 21. Ree1 Qa5  (21...Ba4 $11 
+{ Fritz7 })  22. Ra1 Qxc3 23. Nxc6 bxc6 24. Ba6+ $18) 21. Qf4
+{ This general activity is perfect. It threatens Pf7, defends Nd4 and in some cases prepares for Qf4-b4 to attack Kc8. }
+ (21. Ree1 
+{ Fritz7 }) (21. Nxc6 bxc6 22. Ree1 
+{ Fritz7 }) 21...Rhd8
+{ Black is probably wondering why he organized his pieces to only defend light squares. Only Qa5 and Nc5 can get to dark squares and that makes White's task of coordinating much easier. }
+ (21...Qxa2 
+{ still premature }
+ 22. Nxc6 bxc6 23. Qb4 Nb7 24. Ree1)  (21...Qxc3 $4 22. Nxc6 bxc6 23. Ba6+)  (21...Rc7 $14 
+{ Fritz7 }) (21...Na4 $14 
+{ Fritz7 }) 22. Kg1 (22. Nxc6 bxc6 23. Qb4 Qxb4 24. cxb4 d4 25. Ree1 Na4 $11 
+{ Fritz7 }) 22...Nxd3 23. Rxd3 (23. cxd3 Qxc3 24. Rg3 Rc7 $14 
+{ Fritz7 }) 23...Qc5 (23...Qxa2 24. Rdd1 Qc4 $11 
+{ Fritz7 }) 24. Rb4 a5 $2 (24...Rc7 
+{ Mark and Fritz7 agree! }) 25. Rb1 Rc7 26. Qc1 Be8 27. Nb3 (27. Qb2 
+{ If White commits too quickly to the b-file then Black might actually create some play against Ph4 and on the c-file. }
+ 27...Qe7  (27...a4 $11 
+{ Fritz7 })  28. Nf3 Rc4 
+{ possibly preparing ...b5 }) 27...Qb6 (27...Qc4 28. Nxa5 Qxh4 $14 
+{ Fritz7 }) 28. Nd4
+{ Black created the weakness (Pa5) and can't quite defend it, so Anand forces a draw. }
+1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.13"]
+[Round "5"]
+[White "Wade, Robert"]
+[Black "Littlewood, John Eric"]
+[Result "0-1"]
+[WhiteElo "2365"]
+[BlackElo "2310"]
+[ECO "D80"]
+[EventDate "1971.08.09"]
+[PlyCount "94"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+
+
+1. d4 Nf6 2. c4 g6 3. Nc3 d5 4. Bg5 Ne4 5. Bh4 Bg7 6. e3 c5 7. cxd5 Nxc3 8.
+bxc3 cxd4 9. cxd4 Qxd5 10. Nf3 Nc6 11. Qa4 O-O 12. Qb5 Qe4 13. Be2 e5 14.
+d5 Nb4 15. O-O Nxd5 16. Rac1 a6 17. Qb3 Be6 18. Rc4 Nf4 19. Rxe4 Nxe2+ 20.
+Kh1 Bxb3 21. axb3 b5 22. Be7 Rfe8 23. Bb4 f5 24. Rh4 Bf6 25. Rh3 a5 26. Bd2
+b4 27. e4 fxe4 28. Ng5 Bxg5 29. Bxg5 Rac8 30. Re3 Nd4 31. h4 h6 32. Bxh6
+Nf5 33. Bg5 Nxe3 34. fxe3 Rf8 35. Ra1 Rc3 36. Rxa5 Rxb3 37. Rxe5 Rd3 38.
+Rb5 b3 39. Kg1 Rc8 40. Rb7 Rc2 41. Kh2 b2 42. Bf6 Rdd2 43. Kg3 Rxg2+ 44.
+Kf4 Rcf2+ 45. Ke5 Rg1 46. Rb8+ Kf7 47. Rb7+ Ke8 0-1
+
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.13"]
+[Round "5"]
+[White "Whiteley, Andrew"]
+[Black "Hartston, William R"]
+[Result "1/2-1/2"]
+[WhiteElo "2310"]
+[BlackElo "2390"]
+[ECO "A32"]
+[EventDate "1971.08.09"]
+[PlyCount "31"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. d4 Nf6 2. c4 c5 3. Nf3 cxd4 4. Nxd4 e6 5. Nc3 Bb4 6. Nb5 O-O 7. a3 Bxc3+
+8. Nxc3 d5 9. cxd5 exd5 10. e3 Nc6 11. Be2 Be6 12. O-O Rc8 13. Bd2 d4 14.
+exd4 Nxd4 15. Be3 Nxe2+ 16. Nxe2 1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.13"]
+[Round "5"]
+[White "Wright, Daniel"]
+[Black "Cafferty, Bernard"]
+[Result "1/2-1/2"]
+[WhiteElo "2340"]
+[BlackElo "2440"]
+[ECO "E13"]
+[EventDate "1971.08.09"]
+[PlyCount "38"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. c4 Nf6 2. Nc3 e6 3. Nf3 Bb4 4. d4 b6 5. Bg5 h6 6. Bh4 Bb7 7. e3 g5 8.
+Bg3 Ne4 9. Qc2 Bxc3+ 10. bxc3 d6 11. Bd3 Nxg3 12. hxg3 Nd7 13. Be4 Bxe4 14.
+Qxe4 Ke7 15. Qd3 Qc8 16. Ke2 Qb7 17. Rh2 Nf6 18. Rhh1 Rag8 19. Rhb1 g4
+1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.14"]
+[Round "6"]
+[White "Clarke, Peter H"]
+[Black "Lee, Peter N"]
+[Result "1/2-1/2"]
+[WhiteElo "2380"]
+[BlackElo "2390"]
+[ECO "E80"]
+[EventDate "1971.08.09"]
+[PlyCount "50"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+
+1. d4 Nf6 2. c4 g6 3. Nc3 Bg7 4. e4 d6 5. f3 c6 6. Be3 a6 7. Qd2 b5 8.
+O-O-O Qa5 9. Kb1 Nbd7 10. h4 e5 11. dxe5 dxe5 12. g4 O-O 13. h5 b4 14. Nce2
+Nc5 15. hxg6 fxg6 16. Nc1 Be6 17. Nh3 Rad8 18. Qc2 Rxd1 19. Qxd1 Rd8 20.
+Qc2 Kh8 21. Be2 Bg8 22. Rd1 Rxd1 23. Qxd1 Bf8 24. Qd2 Nb7 25. Nf2 Qc7
+1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.14"]
+[Round "6"]
+[White "Hartston, William R"]
+[Black "Penrose, Jonathan"]
+[Result "1/2-1/2"]
+[WhiteElo "2390"]
+[BlackElo "2450"]
+[ECO "B81"]
+[EventDate "1971.08.09"]
+[PlyCount "42"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. e4 c5 2. Nf3 e6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 d6 6. g4 h6 7. g5 hxg5 8.
+Bxg5 Nc6 9. Qd2 Nxd4 10. Qxd4 Bd7 11. O-O-O Qa5 12. h4 Be7 13. Kb1 O-O-O
+14. Be3 Kb8 15. b4 Qb6 16. Qxb6 axb6 17. f3 Rc8 18. Nb5 Bxb5 19. Bxb5 d5
+20. e5 Nh5 21. a3 Ng3 1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.14"]
+[Round "6"]
+[White "Keene, Raymond"]
+[Black "Whiteley, Andrew"]
+[Result "1-0"]
+[WhiteElo "2465"]
+[BlackElo "2310"]
+[ECO "A05"]
+[EventDate "1971.08.09"]
+[PlyCount "59"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. Nf3 Nf6 2. g3 g6 3. b3 Bg7 4. Bb2 d5 5. c4 O-O 6. Bg2 c5 7. O-O Nc6 8.
+cxd5 Nxd5 9. Bxg7 Kxg7 10. d4 cxd4 11. Nxd4 Ncb4 12. a3 Na6 13. Ra2 Nf6 14.
+Nc3 Qa5 15. b4 Qh5 16. e4 Qxd1 17. Rxd1 e5 18. Ndb5 Bg4 19. f3 Be6 20. Rad2
+Rfc8 21. Nd5 Ne8 22. Bf1 Nb8 23. Ne7 Nc6 24. Nxc8 Rxc8 25. Rc1 Kf8 26. Kf2
+Ke7 27. Nc3 Nd4 28. Nd5+ Kd8 29. Rxc8+ Kxc8 30. Ne3 1-0
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.16"]
+[Round "7"]
+[White "Penrose, Jonathan"]
+[Black "Keene, Raymond"]
+[Result "0-1"]
+[WhiteElo "2450"]
+[BlackElo "2465"]
+[ECO "B09"]
+[EventDate "1971.08.09"]
+[PlyCount "74"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. e4 g6 2. d4 Bg7 3. Nc3 d6 4. f4 Nf6 5. Nf3 O-O 6. Bd3 Nc6 7. h3 Nb4 8.
+Be3 b6 9. a3 Nxd3+ 10. cxd3 c5 11. Qd2 Ba6 12. Kf2 Rc8 13. Rhc1 Nd7 14. d5
+e6 15. dxe6 fxe6 16. Re1 Nf6 17. Rad1 d5 18. exd5 exd5 19. d4 Ne4+ 20. Nxe4
+dxe4 21. Ne5 g5 22. Kg1 gxf4 23. Bxf4 Qxd4+ 24. Qxd4 cxd4 25. g3 Rfe8 26.
+Rxe4 Rc5 27. Rde1 d3 28. Nf3 Rxe4 29. Rxe4 Bb7 30. Re3 d2 31. Nxd2 Bd4 32.
+Kf1 Ba6+ 33. Kg2 Bxe3 34. Bxe3 Rc2 35. b3 Bb7+ 36. Kf1 Bd5 37. Ke1 Bxb3 0-1
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.16"]
+[Round "7"]
+[White "Wright, Daniel"]
+[Black "Hartston, William R"]
+[Result "1/2-1/2"]
+[WhiteElo "2340"]
+[BlackElo "2390"]
+[ECO "B36"]
+[EventDate "1971.08.09"]
+[PlyCount "81"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. c4 Nf6 2. Nc3 g6 3. e4 c5 4. Nf3 Nc6 5. d4 cxd4 6. Nxd4 d6 7. Be2 Nxd4
+8. Qxd4 Bg7 9. O-O O-O 10. Qd3 Be6 11. Bd2 Nd7 12. b3 a6 13. Rac1 Rb8 14.
+Qe3 Ne5 15. Rcd1 Bd7 16. h3 b5 17. f4 Nc6 18. Be1 Nb4 19. Bh4 Re8 20. Rd2
+Qb6 21. Qxb6 Rxb6 22. e5 bxc4 23. Bxc4 Be6 24. Bxe6 fxe6 25. a3 Nc6 26. b4
+dxe5 27. Na4 Rb5 28. Nc5 exf4 29. Nxa6 Bd4+ 30. Bf2 Bxf2+ 31. Rfxf2 Rb6 32.
+b5 e5 33. Rd5 Rxa6 34. bxa6 Ra8 35. a7 Rxa7 36. Rc5 Ra6 37. Rfc2 Nd4 38.
+Ra2 Nc6 39. Rac2 Nd4 40. Ra2 Nc6 41. a4 1/2-1/2
+
+[Event "GBR-ch 58th"]
+[Site "Blackpool"]
+[Date "1971.08.17"]
+[Round "8"]
+[White "Keene, Raymond"]
+[Black "Littlewood, John Eric"]
+[Result "1/2-1/2"]
+[WhiteElo "2465"]
+[BlackElo "2310"]
+[ECO "A49"]
+[EventDate "1971.08.09"]
+[PlyCount "76"]
+[EventType "swiss"]
+[EventRounds "11"]
+[EventCountry "GBR"]
+
+1. Nf3 Nf6 2. g3 g6 3. b3 Bg7 4. Bb2 O-O 5. Bg2 d6 6. d4 c6 7. O-O Nbd7 8.
+Nbd2 Qc7 9. e4 Re8 10. c4 e5 11. d5 c5 12. Qc2 Rf8 13. a3 Ng4 14. Rad1 Qd8
+15. Nh4 Ndf6 16. Ndf3 Ne8 17. Bc1 Rb8 18. Bd2 Nc7 19. h3 Nf6 20. Rb1 Nfe8
+21. b4 Bd7 22. Rfe1 b6 23. Kh2 Kh8 24. Ng5 f6 25. Ngf3 Rc8 26. bxc5 dxc5
+27. Bc3 Nd6 28. Nd2 Bh6 29. Nhf3 Nf7 30. Re2 Ne8 31. a4 Ned6 32. Ne1 Kg8
+33. Nd3 Qe8 34. Ra1 a6 35. Ree1 b5 36. axb5 axb5 37. cxb5 Bxb5 38. Nb2 f5
+1/2-1/2
+"""
