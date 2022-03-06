8294357436c101529f5e10cb80008ed48642d3de
eapDimitriosCollier <dimitrios.collier@gmail.com>
2022-03-06 13:07:40 +0200

    FileExplorer instructions

--- FIleExplorer/file explorer .py
+++ FIleExplorer/file explorer .py
@@ -3,62 +3,69 @@
   
 # import all components
 # from the tkinter library
-from tkinter import *
+# from tkinter import *
   
-# import filedialog module
-from tkinter import filedialog
+# # import filedialog module
+# from tkinter import filedialog
   
-# Function for opening the
-# file explorer window
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
+# # Function for opening the
+# # file explorer window
+# def browseFiles():
+#     filename = filedialog.askopenfilename(initialdir = "/",
+#                                           title = "Select a File",
+#                                           filetypes = (("Text files",
+#                                                         "*.txt*"),
+#                                                        ("all files",
+#                                                         "*.*")))
       
-    # Change label contents
-    label_file_explorer.configure(text="File Opened: "+filename)
+#     # Change label contents
+#     label_file_explorer.configure(text="File Opened: "+filename)
       
       
                                                                                                   
-# Create the root window
-window = Tk()
+# # Create the root window
+# window = Tk()
   
-# Set window title
-window.title('File Explorer')
+# # Set window title
+# window.title('File Explorer')
   
-# Set window size
-window.geometry("500x500")
+# # Set window size
+# window.geometry("500x500")
   
-#Set window background color
-window.config(background = "white")
+# #Set window background color
+# window.config(background = "white")
   
-# Create a File Explorer label
-label_file_explorer = Label(window,
-                            text = "File Explorer using Tkinter",
-                            width = 100, height = 4,
-                            fg = "blue")
+# # Create a File Explorer label
+# label_file_explorer = Label(window,
+#                             text = "File Explorer using Tkinter",
+#                             width = 100, height = 4,
+#                             fg = "blue")
   
       
-button_explore = Button(window,
-                        text = "Browse Files",
-                        command = browseFiles)
+# button_explore = Button(window,
+#                         text = "Browse Files",
+#                         command = browseFiles)
   
-button_exit = Button(window,
-                     text = "Exit",
-                     command = exit)
+# button_exit = Button(window,
+#                      text = "Exit",
+#                      command = exit)
   
-# Grid method is chosen for placing
-# the widgets at respective positions
-# in a table like structure by
-# specifying rows and columns
-label_file_explorer.grid(column = 1, row = 1)
+# # Grid method is chosen for placing
+# # the widgets at respective positions
+# # in a table like structure by
+# # specifying rows and columns
+# label_file_explorer.grid(column = 1, row = 1)
   
-button_explore.grid(column = 1, row = 2)
+# button_explore.grid(column = 1, row = 2)
   
-button_exit.grid(column = 1,row = 3)
+# button_exit.grid(column = 1,row = 3)
   
-# Let the window wait for any events
-window.mainloop()
+# # Let the window wait for any events
+# window.mainloop()
+
+# Μια κλάση FileExplorer
+# με __init__ που είναι υπεύθυνο για να κάνει
+# initialization του tkinter
+# και μια μέθοδο show η οποία θα παρουσιάζει τον FileExplorer
+# Επίσης θέλουμε κατά την επιλογή ενός αρχείου, τα περιεχόμενά του
+# να αποθηκεύονται σε ένα class property.
