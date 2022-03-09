8e52f8e6a1911dd0b75b797fd177214413982a01
george partsinevelos <georgepartsinevelos12@gmail.com>
2022-03-09 23:45:41 +0200

    File Explorer small corrections

--- FIleExplorer/FileExplorer.py
+++ FIleExplorer/FileExplorer.py
@@ -5,17 +5,16 @@ import os
 class FileExplorer:
     def __init__(self):
         self.dataFile = None
-        self.window = None
 
     def open(self):
-        window = Tk()
         filepath = filedialog.askopenfilename(initialdir=os.getcwd(), title="Please select a file", filetypes= [("PGN files","*.pgn")])
 
         self.file = open(filepath,'r')
         self.dataFile = self.file.read()
         self.file.close()
         
-        window.mainloop()
+        return self.dataFile
 
-# How to use:
-# FileExplorer().open()
+# # How to use:
+
+#     print(FileExplorer().open())
