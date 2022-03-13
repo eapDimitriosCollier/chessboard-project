360cc9874407360b013edb797ef46fb82fb3c45a
Panagiotis A <pargyropoulos@gmail.com>
2022-03-13 21:06:23 +0200

    fixes

--- GUI_PA/Sound.py
+++ GUI_PA/Sound.py
@@ -5,13 +5,14 @@ sys.path.append('../CHESSBOARD-PROJECT')
 from playsound import playsound
 MoveWAV = os.getcwd()+u'/GUI_PA/sound/Move.wav'
 CheckWAV = os.getcwd()+u'/GUI_PA/sound/Check.wav'
-CaptureWAV = os.getcwd()+u'/GUI_PA/sound/Capture.wav'
+CaptureWAV = os.getcwd()+u'/GUI_PA/sound/Cap.wav'
 
 
 def PlayWAV(wav_file:str)->None:
-    
     playsound(wav_file)
 
 
-#PlayWAV(wavFile)
-print (playsound.__doc__)
+if __name__ == '__main__':
+    PlayWAV(MoveWAV)
+    PlayWAV(CheckWAV)
+    PlayWAV(CaptureWAV)
