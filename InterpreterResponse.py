0185214baa7d4849fc4225ad0c73743ee16544f0
Panagiotis A <pargyropoulos@gmail.com>
2022-06-07 23:37:04 +0300

    Refactoring App Structure

--- InterpreterResponse.py
+++
@@ -1,6 +0,0 @@
-from Response.Response import Response
-
-class InterpreterResponse(Response):
-    def __init__(self) -> None:
-        super().__init__()
-    
--- RequestListener/RequestListener.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from Request.Request import Request
-
-class RequestListener(ABC):
-    @abstractmethod
-    def onRequest(self, request: Request):
-        pass
